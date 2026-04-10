824b1db67e48049c4193a591d988d89c0b5f556f
akv-dev339 <akshayvermaakv07@gmail.com>
2026-04-10 18:54:02 +0530

    responsive topbar

--- components/layout/Topbar.tsx
+++ components/layout/Topbar.tsx
@@ -1,12 +1,20 @@
 export default function TopBar() {
   return (
-    <div className="bg-[#020617] text-cyan-300 text-xs md:text-sm flex justify-between px-6 md:px-12 py-2 border-b border-cyan-900">
-      
-      <span>Registered No: INDO141229SE019665</span>
+    <div className="bg-[#020617] text-cyan-300 text-xs md:text-sm border-b border-cyan-900">
 
-      <div className="flex gap-6">
-        <span>drlindore25@gmail.com </span>
-        <span>+91-9691528102</span>
+      <div className="max-w-7xl mx-auto px-4 md:px-12 py-2 flex flex-col md:flex-row items-center justify-between gap-2 md:gap-0">
+
+        {/* LEFT */}
+        <span className="text-center md:text-left">
+          Registered No: INDO141229SE019665
+        </span>
+
+        {/* RIGHT */}
+        <div className="flex flex-col sm:flex-row items-center gap-1 sm:gap-6 text-center md:text-right">
+          <span>📧 drlindore25@gmail.com</span>
+          <span>📞 +91-9691528102</span>
+        </div>
+
       </div>
 
     </div>
